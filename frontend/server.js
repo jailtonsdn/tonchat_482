c56fbf67a0b1969362a7e24320d085c4ee247ceb
Jailton Nascimento <108580162+jailtonsdn@users.noreply.github.com>
2024-07-25 09:10:48 -0300

    Update server.js

--- frontend/server.js
+++ frontend/server.js
@@ -1,10 +1,12 @@
-//simple express server to run frontend production build;
 const express = require("express");
 const path = require("path");
 const app = express();
 app.use(express.static(path.join(__dirname, "build")));
 app.get("/*", function (req, res) {
-	res.sendFile(path.join(__dirname, "build", "index.html"));
+    res.sendFile(path.join(__dirname, "build", "index.html"));
 });
-app.listen(3250);
 
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Frontend server is running on port ${port}`);
+});
